2a0b4b4bfb0da58393ccda8f190b4e0ce7d82da4
chebon254 <kelvinchebon90@gmail.com>
2023-06-12 20:11:38 +0300

    code changes

--- Insurance/js/scripts.js
+++ Insurance/js/scripts.js
@@ -9,14 +9,37 @@ function dropOne() {
 
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (!event.target.matches('.drp-action-btn')) {
-      let dropdowns = document.getElementsByClassName("drop-dwn-menu");
-      for (let i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.style.display = "block") {
-            openDropdown.style.display = "none";
+        let dropdowns = document.getElementsByClassName("drop-dwn-menu");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.style.display = "block") {
+                openDropdown.style.display = "none";
+            }
         }
-      }
     }
-  }
+}
+/* == || Drop Down == */
+
+/* ======== TABS ========== */
+// JavaScript
+function changeTab(event, tabId) {
+    // Get all tab buttons and tab contents
+    var tabButtons = document.getElementsByClassName('tab-button');
+    var tabContents = document.getElementsByClassName('tab-content');
+
+    // Remove 'active' class from all tab buttons and tab contents
+    for (var i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove('active');
+    }
+    for (var i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove('active');
+    }
+
+    // Add 'active' class to the selected tab button and tab content
+    event.currentTarget.classList.add('active');
+    document.getElementById(tabId).classList.add('active');
+}
+/* ======== TABS ========== */
+
