14b4cfa511edcbca8b1a26bd6acb0dacf92957d0
chebon254 <kelvinchebon90@gmail.com>
2023-06-19 13:53:41 +0300

    code changes

---
+++ Insurance/js/inbox.js
@@ -0,0 +1,18 @@
+/* ======== Messages Tab ========= */ 
+function openMessaging(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("msgtabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("msgtablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+/* ======== || Messages Tab ========= */ 
