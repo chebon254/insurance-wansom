5e4ebfe86dbf91af2e48d05ca1dfbfaecd2a35bf
chebon254 <kelvinchebon90@gmail.com>
2023-06-19 18:19:46 +0300

    code changes

--- Insurance/js/inbox.js
+++ Insurance/js/inbox.js
@@ -1,18 +1,28 @@
-/* ======== Messages Tab ========= */ 
+/* ======== Messages Tab ========= */
 function openMessaging(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("msgtabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("msgtablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
-/* ======== || Messages Tab ========= */ 
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+/* ======== || Messages Tab ========= */
+
+/* ===== PROFILE REVEAL ==== */
+// Onclick For Menu Bar
+// var menuBtn = document.getElementsByClassName('internal-btn');
+// var menu = document.getElementsByClassName('inbox-msg-profile');
+// menuBtn.onclick = () => {
+//     menu.classList.add("active");
+//     console.log("Button Clicked");
+// }
+/* ===== PROFILE REVEAL ==== */
